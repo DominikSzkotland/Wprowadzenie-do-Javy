303ed63e054879427b6acd13932afb4fdab68b7d
DominikSzkotland <dszlencka@outlook.com>
2023-09-06 15:00:56 +0200

    wylosowanie liczby

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Zostanie wylosowana jedna liczba z zakresu od 1 do 100");
+        int liczbaWylosowana = (int)(Math.random()*100+1);
+        System.out.println(liczbaWylosowana);
+        /*
+        rzutowanie z double do int
+        rzutowanie zawężające
+        rzutowanie to takie odcinanie tego co po przecinku
+
+
+        typy proste
+        boolean
+        char
+
+        byte
+        int
+
+        byte do int = rzutowanie z którego nic nie tracę
+        int do byte = rzutowanie zawężające
+         */
+
     }
+
 }
