c636074d1ffa88b6d246b5dcdaa2bcc4207cdd0c
DominikSzkotland <dszlencka@outlook.com>
2023-09-06 17:11:48 +0200

    Metody losuj wczytywanie i trafione

--- src/Main.java
+++ src/Main.java
@@ -4,62 +4,53 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-    private static HashSet<Integer> WylosujLiczby() {
-    System.out.println("Zostanie wylosowanych 6 liczb z zakresu od 1 do 100");
-    HashSet<Integer> wylosowane = new HashSet<>();
-    while (wylosowane.size() < 6) {
-        wylosowane.add((int) (Math.random() * 10 + 1));
-    }
-    return wylosowane;
-}
-    public static void main(String[] args) {
+
+    private static HashSet<Integer> wylosujLiczby(){
         System.out.println("Zostanie wylosowanych 6 liczb z zakresu od 1 do 100");
         HashSet<Integer> wylosowane = new HashSet<>();
-        /*
-        kolekcje:
-        listy - ArrayList, LinkedList - elementy uporządkowane indeksowane mogą się powtarzać
-        zbiory - HashSet - elementy zazwyczaj nieporządkowane nieindeksowane nie mogą się potarzać
-        mapy
-         */
-        while (wylosowane.size() < 6) {
-            wylosowane.add((int) (Math.random() * 10 + 1));
+        while(wylosowane.size()<6){
+
+            wylosowane.add((int) (Math.random() * 100 + 1));
         }
-        System.out.println(wylosowane);//adres obiektu
-        for (int wylosowana : wylosowane)//taki foreach
-        {
-            System.out.println(wylosowana + ", ");
-        }
-        //wczytywać z klawaitury 6 liczb bez powtórzeń
+        return wylosowane;
+    }
+    private static ArrayList<Integer> wczytajLiczby(){
+        //Wczytywać z klawiatury 6 liczb bez powtorzen
         Scanner klawiatura = new Scanner(System.in);
         ArrayList<Integer> liczbyWczytane = new ArrayList<>();
-
-        for (int i = 0; i < 6; i++) {
+        for(int i=0;i<6;i++){
             int liczba;
             do {
-                System.out.println("podaj liczbę");
+                System.out.println("Podaj liczbę");
                 liczba = klawiatura.nextInt();
-            }
-            while (liczbyWczytane.contains(liczba));
-            {
-                liczbyWczytane.add(liczba);
-            }
-        }
-        System.out.println("Wczytane liczby" + liczbyWczytane);
 
+            }while (liczbyWczytane.contains(liczba));
+            liczbyWczytane.add(liczba);
+        }
+        return liczbyWczytane;
+    }
+    private static LinkedList<Integer> sprawdzWyniki(HashSet<Integer>wylosowane,
+                                                     ArrayList<Integer>liczbyWczytane){
         LinkedList<Integer> trafione = new LinkedList<>();
         //jeżeli jakaś liczba będzie i w wylosowane i w wpisane to dać do trafione
-        for (Integer wylosowana : wylosowane) {
-            if (liczbyWczytane.contains(wylosowana)) {
+        for(Integer wylosowana:wylosowane ){
+            if(liczbyWczytane.contains(wylosowana)){
                 trafione.add(wylosowana);
             }
         }
-        System.out.println("Trafiono " + trafione);
-
-        //różnica między typami prostymi i złożonymi
-        int liczba1 = 20; //typy proste nie mają metod.
-        Integer liczba2 = 30; //typy złożone zawsze pisane z dużej litery obiekty które mają własnośći i metody
+        return trafione;
     }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        HashSet<Integer> wylosowane = wylosujLiczby();
+
+        ArrayList<Integer> liczbyWczytane = wczytajLiczby();
+
+        LinkedList<Integer> trafione = sprawdzWyniki(wylosowane,liczbyWczytane);
+
+        System.out.println("Trafiono "+trafione);
 
 
-
+    }
 }
